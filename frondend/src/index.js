44a9172af1479d287163449dc4234fef5b866ecc
Muhammed Shafeeque P <shafequekkv95@gmial.com>
2023-03-26 08:14:07 +0530

    updated

--- frondend/src/index.js
+++ frondend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {ChakraProvider} from '@chakra-ui/react'
-
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,4 +14,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
